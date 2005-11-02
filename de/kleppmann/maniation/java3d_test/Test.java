1afabe1af2a09e19dcfefcf0fedbe21eca16bc72
Martin Kleppmann <martin@kleppmann.de>
2005-11-02 19:30:08 +0000

    now displays a yellow triangle. pretty.
    git-svn-id: file:///Users/martin/Documents/svnrepo/trunk/martin/graphics/maniation@1044 3ab4d77f-209f-454f-af32-ffe4b78d429c

--- de/kleppmann/maniation/java3d_test/Test.java
+++ de/kleppmann/maniation/java3d_test/Test.java
@@ -3,10 +3,12 @@ package de.kleppmann.maniation.java3d_test;
 import java.awt.Dimension;
 import javax.media.j3d.BranchGroup;
 import javax.media.j3d.Canvas3D;
+import javax.media.j3d.GeometryArray;
+import javax.media.j3d.Shape3D;
 import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
+import javax.media.j3d.TriangleArray;
 import javax.swing.JFrame;
-import com.sun.j3d.utils.geometry.ColorCube;
 import com.sun.j3d.utils.universe.SimpleUniverse;
 
 public class Test extends JFrame {
@@ -21,16 +23,29 @@ public class Test extends JFrame {
         SimpleUniverse universe = new SimpleUniverse(canvas);
         universe.getViewingPlatform().setNominalViewingTransform();
         
+        GeometryArray geometry = new TriangleArray(3, TriangleArray.COORDINATES |
+                TriangleArray.COLOR_3);
+        double[] a = {0.0d, 0.0d, 0.0d};
+        double[] b = {0.3d, 0.0d, 0.0d};
+        double[] c = {0.3d, 0.3d, 0.0d};
+        geometry.setCoordinate(0, a);
+        geometry.setCoordinate(1, b);
+        geometry.setCoordinate(2, c);
+        float[] col = {1.0f, 1.0f, 0.0f};
+        geometry.setColor(0, col);
+        geometry.setColor(1, col);
+        geometry.setColor(2, col);
+        Shape3D shape = new Shape3D();
+        shape.setGeometry(geometry);
+        //Transform3D rotX = new Transform3D();
+        //Transform3D rotY = new Transform3D();
+        //rotX.rotX(Math.PI/4.0);
+        //rotY.rotY(Math.PI/5.0);
+        //rotX.mul(rotY);
+        //TransformGroup cubeTransform = new TransformGroup(rotX);
+        //cubeTransform.addChild(shape);
         BranchGroup cube = new BranchGroup();
-        ColorCube cubeMesh = new ColorCube(0.4);
-        Transform3D rotX = new Transform3D();
-        Transform3D rotY = new Transform3D();
-        rotX.rotX(Math.PI/4.0);
-        rotY.rotY(Math.PI/5.0);
-        rotX.mul(rotY);
-        TransformGroup cubeTransform = new TransformGroup(rotX);
-        cube.addChild(cubeTransform);
-        cubeTransform.addChild(cubeMesh);
+        cube.addChild(shape);
         cube.compile();
         
         universe.addBranchGraph(cube);
