66f10d0c8160b065688a96320f5735b32a5f573e
Martin Kleppmann <martin@kleppmann.de>
2005-11-24 14:54:11 +0000

    Fixes
    git-svn-id: file:///Users/martin/Documents/svnrepo/trunk/martin/graphics/maniation@1087 3ab4d77f-209f-454f-af32-ffe4b78d429c

--- matlab/normalize.m
+++ matlab/normalize.m
@@ -1,4 +1,4 @@
 function retval = normalize(vector)
     % Normalize a vector or quaternion to have unit length (magnitude).
-    retval = vector / sumsq(vector)
+    retval = vector / sumsq(vector);
 endfunction
