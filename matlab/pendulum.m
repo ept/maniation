abe5836d548f7e2164f1f6bccb9edf2a8cfc5ec2
Martin Kleppmann <martin@kleppmann.de>
2005-12-21 15:41:54 +0000

    Pendulum is working!
    git-svn-id: file:///Users/martin/Documents/svnrepo/trunk/martin/graphics/maniation@1137 3ab4d77f-209f-454f-af32-ffe4b78d429c

--- matlab/pendulum.m
+++ matlab/pendulum.m
@@ -1,8 +1,8 @@
 function [inertia, invInertia, initStatus, constrFuncs, forcesAndTorques] = pendulum(status)
     inertia = eye(6);
     invInertia = eye(6);
-    initStatus = [0;0;0;0;0;0;1;0;0;0;0;0;0.5];
+    initStatus = [1/sqrt(2);1-1/sqrt(2);0;qrotz(pi/4);0;0;0;0;0;0];
     constrFuncs = cell(1,1);
     constrFuncs(1) = str2func("nail");
-    forcesAndTorques = [0;0;0;0;0;0];
+    forcesAndTorques = [0;-1;0;0;0;0];
 endfunction
