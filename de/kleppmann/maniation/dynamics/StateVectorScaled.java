5fcfe0c7ed5104d39221c64af3b9b4e1a44e1145
Martin Kleppmann <martin@kleppmann.de>
2006-01-02 15:50:04 +0000

    Fixed a bug which occurred when an instance of StateVectorModified was multiplied by a scalar.
    git-svn-id: file:///Users/martin/Documents/svnrepo/trunk/martin/graphics/maniation@1179 3ab4d77f-209f-454f-af32-ffe4b78d429c

--- de/kleppmann/maniation/dynamics/StateVectorScaled.java
+++ de/kleppmann/maniation/dynamics/StateVectorScaled.java
@@ -4,19 +4,21 @@ import de.kleppmann.maniation.maths.Vector;
 
 class StateVectorScaled extends StateVector {
     
+    private StateVector origin;
     private double factor;
     
     public StateVectorScaled(StateVector origin, double factor) {
         super(origin.getScene());
         setDerivative(origin.isDerivative());
+        this.origin = origin;
         this.factor = factor;
     }
 
     public double getComponent(int index) {
-        return factor*super.getComponent(index);
+        return factor*origin.getComponent(index);
     }
 
     public Vector mult(double scalar) {
-        return new StateVectorScaled(this, factor*scalar);
+        return new StateVectorScaled(origin, factor*scalar);
     }
 }
