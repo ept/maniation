2c0b2eecaa2f7b8240ca40d9dc4852495b5ea9fc
Martin Kleppmann <martin@kleppmann.de>
2006-02-17 22:34:21 +0000

    Try lower step sizes
    git-svn-id: file:///Users/martin/Documents/svnrepo/trunk/martin/graphics/maniation@1261 3ab4d77f-209f-454f-af32-ffe4b78d429c

--- de/kleppmann/maniation/maths/RungeKutta.java
+++ de/kleppmann/maniation/maths/RungeKutta.java
@@ -4,7 +4,7 @@ public class RungeKutta implements ODESolver {
     
     private ODE ode;
     private double time, timeStepGuess;
-    private double h, hmin = 1e-3, eps = 1e-6;
+    private double h, hmin = 1e-4, eps = 1e-6;
     private Vector status;
 
     public RungeKutta(ODE ode, double timeStepGuess) {
