ebc9b4d3cfa50a7b2a78030719b64bf78baffab4
Martin Kleppmann <martin@kleppmann.de>
2006-02-27 22:25:22 +0000

    Use DynamicScene, and load `boxes' example from XML file.
    git-svn-id: file:///Users/martin/Documents/svnrepo/trunk/martin/graphics/maniation@1311 3ab4d77f-209f-454f-af32-ffe4b78d429c

--- de/kleppmann/maniation/TestDynamics.java
+++ de/kleppmann/maniation/TestDynamics.java
@@ -1,15 +1,26 @@
 package de.kleppmann.maniation;
 
+import de.kleppmann.maniation.dynamics.DynamicScene;
 import de.kleppmann.maniation.dynamics.Simulation;
+import de.kleppmann.maniation.scene.Scene;
+import de.kleppmann.maniation.scene.XMLParser;
 
 public class TestDynamics {
 
     public TestDynamics() {
-        super();
+        XMLParser sceneParser = new XMLParser(new org.apache.xerces.jaxp.SAXParserFactoryImpl());
+        Scene scene;
+        try {
+            scene = sceneParser.parse("../boxes.xml");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
         Simulation sim = new Simulation();
         //MultiPendulum.setup(sim, 2);
-        new NewtonsCradle(sim);
-        sim.run(1.0);
+        //new NewtonsCradle(sim);
+        new DynamicScene(scene, sim);
+        sim.run(5.0);
     }
 
     public static void main(String[] args) {
