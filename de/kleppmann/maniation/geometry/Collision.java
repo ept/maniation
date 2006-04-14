dfc73fb697bc03270f5ca99579d7951abe378b30
Martin Kleppmann <martin@kleppmann.de>
2006-04-14 21:49:53 +0000

    Revert compound collision handling back to plane approximation, because the other methods didn't work. Enhanced it by using the point-in-volume feature though. Collisions between an articulated limb and another limb or a mesh are now handled by bounding spheres (bubbles) along the length of the limb. If such a type of collision is detected, an appropriate spherical constraint is generated.
    git-svn-id: file:///Users/martin/Documents/svnrepo/trunk/martin/graphics/maniation@1484 3ab4d77f-209f-454f-af32-ffe4b78d429c

--- de/kleppmann/maniation/geometry/Collision.java
+++ de/kleppmann/maniation/geometry/Collision.java
@@ -2,13 +2,17 @@ package de.kleppmann.maniation.geometry;
 
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import de.kleppmann.maniation.dynamics.EdgeEdgeCollision;
+import de.kleppmann.maniation.dynamics.InequalityConstraint;
 import de.kleppmann.maniation.dynamics.InteractionList;
 import de.kleppmann.maniation.dynamics.Body;
+import de.kleppmann.maniation.dynamics.SphereEdgeCollision;
+import de.kleppmann.maniation.dynamics.SphereFaceCollision;
+import de.kleppmann.maniation.dynamics.SphereSphereCollision;
 import de.kleppmann.maniation.dynamics.VertexFaceCollision;
+import de.kleppmann.maniation.maths.Quaternion;
 import de.kleppmann.maniation.maths.Vector3D;
 import de.realityinabox.util.CommutativePair;
 import de.realityinabox.util.Pair;
@@ -19,7 +23,7 @@ public class Collision {
     private final AnimateMesh mesh1, mesh2;
     private final Body body1, body2;
     private Set<MeshTriangle> body1Triangles, body2Triangles;
-    private Body planeBody = null;
+    //private Body planeBody = null;
     private MeshVertex vfVertex = null;
     private MeshTriangle vfFace = null;
     private Vector3D vfNormal = null;
@@ -60,6 +64,8 @@ public class Collision {
         }
         // Test if we can determine a particular type of collision; if not, just
         // approximate the lot with a plane.
+        if (detectLimbLimb(result)) return;
+        if (detectLimbMesh(result)) return;
         if (detectVertexFace()) {
             Body vertexBody = (vfFace.getBody() == body1) ? body2 : body1;
             result.addInteraction(new VertexFaceCollision(vertexBody, vfVertex.getPosition(),
@@ -74,46 +80,80 @@ public class Collision {
     }
     
     private void compoundContact(InteractionList result) {
-        // The more coarse mesh will be used for the faces, to minimize the number of constraints
-        Set<MeshTriangle> planeTriangles, vertexTriangles;
-        Body vertexBody; AnimateMesh planeMesh;
-        if (body1Triangles.size() < body2Triangles.size()) {
-            planeBody = body1; vertexBody = body2; planeMesh = mesh1;
-            planeTriangles = body1Triangles; vertexTriangles = body2Triangles;
-        } else {
-            planeBody = body2; vertexBody = body1; planeMesh = mesh2;
-            planeTriangles = body2Triangles; vertexTriangles = body1Triangles;
+        Vector3D planePoint, planeNormal;
+        // Find a point on the plane by averaging all collision line endpoints
+        Set<CommutativePair<InexactPoint>> lines = new java.util.HashSet<CommutativePair<InexactPoint>>();
+        for (CollisionPoint coll : intersections) {
+            lines.add(new CommutativePair<InexactPoint>(new InexactPoint(coll.lineFrom),
+                    new InexactPoint(coll.lineTo)));
         }
-        // Determine which of the other mesh's vertices lie inside the face mesh volume
-        Set<MeshVertex> inside = new java.util.HashSet<MeshVertex>();
-        Set<MeshVertex> outside = new java.util.HashSet<MeshVertex>();
-        for (MeshTriangle tri : vertexTriangles) {
-            for (MeshVertex vert : tri.vertices) {
-                if (inside.contains(vert) || outside.contains(vert)) continue;
-                if (planeMesh.isInsideVolume(vert.getPosition())) inside.add(vert);
-                else outside.add(vert);
-            }
+        planePoint = new Vector3D();
+        for (CommutativePair<InexactPoint> line : lines) {
+            planePoint = planePoint.add(line.getLeft ().getPosition());
+            planePoint = planePoint.add(line.getRight().getPosition());
         }
-        // Associate each penetrated vertex with the face closest to it.
-        // But for each face, choose the furthest associated vertex.
-        Map<MeshTriangle, VertexFaceCollision> faceColl = new java.util.HashMap<MeshTriangle, VertexFaceCollision>();
-        for (MeshVertex vert : inside) {
-            VertexFaceCollision vfmax = null; double pmax = 0.0; MeshTriangle mtmax = null;
-            for (MeshTriangle face : planeTriangles) {
-                VertexFaceCollision vf = new VertexFaceCollision(vertexBody, vert.getPosition(),
-                        planeBody, face.getVertices()[0].getPosition(), face.getNormal());
-                double pen = vf.getPenalty().getComponent(0);
-                if (((vfmax == null) || (pen > pmax)) && (pen < 0.0)) {
-                    vfmax = vf; pmax = pen; mtmax = face;
+        planePoint = planePoint.mult(0.5/lines.size());
+        // Find the plane normal by averaging the normals (as specified in the file) of all triangles
+        planeNormal = new Vector3D();
+        for (CollisionPoint coll : intersections) {
+            planeNormal = planeNormal.add(coll.tri1.getNormal()).subtract(coll.tri2.getNormal());
+        }
+        if (planeNormal.magnitude() < 1e-6) return;
+        planeNormal = planeNormal.normalize();
+        // Find the point of maximum penetration through the plane
+        MeshVertex vert = null; Body vertexBody = null; double dist = 0.0;
+        for (CollisionPoint coll : intersections) {
+            for (MeshVertex v : coll.getTri1().getVertices()) {
+                if (mesh2.isInsideVolume(v.getPosition())) {
+                    double d = Math.abs(v.getPosition().subtract(planePoint).mult(planeNormal));
+                    if (d > dist) {
+                        vert = v; vertexBody = body1; dist = d;
+                    }
                 }
             }
-            VertexFaceCollision prev = faceColl.get(mtmax);
-            if ((vfmax != null) && ((prev == null) || (prev.getPenalty().getComponent(0) > pmax)))
-                faceColl.put(mtmax, vfmax);
+            for (MeshVertex v : coll.getTri2().getVertices()) {
+                if (mesh1.isInsideVolume(v.getPosition())) {
+                    double d = Math.abs(v.getPosition().subtract(planePoint).mult(planeNormal));
+                    if (d > dist) {
+                        vert = v; vertexBody = body2; dist = d;
+                    }
+                }
+            }
         }
-        for (Map.Entry<MeshTriangle, VertexFaceCollision> entry : faceColl.entrySet()) {
-            result.addInteraction(entry.getValue());
+        // Create vertex/face constraint using the plane
+        if (vert != null) {
+            VertexFaceCollision vf = new VertexFaceCollision(vertexBody, vert.getPosition(),
+                    (vertexBody == body1 ? body2 : body1), planePoint, planeNormal);
+            result.addInteraction(vf);
         }
+        // Find all edges in both bodies which have an intersected triangle on either side
+        Set<Edge> body1Edges = busyEdges(body1Triangles), body2Edges = busyEdges(body2Triangles);
+        // Pair them up and choose one to be an edge/edge constraint
+        EdgeEdgeCollision ee = null; dist = 0.0;
+        for (Edge edge1 : body1Edges) {
+            Vector3D e1 = edge1.getLeft().getPosition();
+            Vector3D e2 = edge1.getRight().getPosition();
+            Vector3D dir1 = e2.subtract(e1);
+            for (Edge edge2 : body2Edges) {
+                Vector3D f1 = edge2.getLeft().getPosition();
+                Vector3D f2 = edge2.getRight().getPosition();
+                Vector3D dir2 = f2.subtract(f1);
+                if (linesProximity(e1, e2, f1, f2)) {
+                    Vector3D normal = dir1.cross(dir2).normalize();
+                    Vector3D oneToTwo = edge1.firstTri.getNormal().add(edge1.secondTri.getNormal());
+                    oneToTwo = oneToTwo.subtract(edge2.firstTri.getNormal()).subtract(edge2.secondTri.getNormal());
+                    if (normal.mult(oneToTwo) < 0.0) {
+                        normal = normal.mult(-1.0); dir2 = dir2.mult(-1.0);
+                    }
+                    double d = f1.subtract(e1).mult(normal);
+                    if (d < dist) {
+                        ee = new EdgeEdgeCollision(body1, e1, dir1, body2, f1, dir2);
+                        dist = d;
+                    }
+                }
+            }
+        }
+        if (ee != null) result.addInteraction(ee);
     }
     
     private boolean detectVertexFace() {
@@ -168,6 +208,82 @@ public class Collision {
         return true;
     }
     
+    private boolean detectLimbLimb(InteractionList result) {
+        if (!(mesh1 instanceof ArticulatedLimb)) return false;
+        if (!(mesh2 instanceof ArticulatedLimb)) return false;
+        ArticulatedLimb limb1 = (ArticulatedLimb) mesh1;
+        ArticulatedLimb limb2 = (ArticulatedLimb) mesh2;
+        Quaternion orient1 = limb1.getCurrentOrientation().getInverse();
+        Quaternion orient2 = limb2.getCurrentOrientation().getInverse();
+        Vector3D base1 = limb1.getDynamicState().getCoMPosition();
+        Vector3D base2 = limb2.getDynamicState().getCoMPosition();
+        SphereSphereCollision coll = null; double dist = 1e40; 
+        for (Pair<Vector3D, Double> bubble1 : limb1.getBubbles()) {
+            Vector3D centre1 = orient1.transform(bubble1.getLeft().subtract(base1));
+            for (Pair<Vector3D, Double> bubble2 : limb2.getBubbles()) {
+                Vector3D centre2 = orient2.transform(bubble2.getLeft().subtract(base2));
+                double d = bubble2.getLeft().subtract(bubble1.getLeft()).magnitude() -
+                        bubble1.getRight() - bubble2.getRight();
+                if (d < dist) {
+                    dist = d;
+                    coll = new SphereSphereCollision(body1, centre1, bubble1.getRight(),
+                            body2, centre2, bubble2.getRight());
+                }
+            }
+        }
+        if (coll == null) return false;
+        result.addInteraction(coll);
+        return true;
+    }
+    
+    private boolean detectLimbMesh(InteractionList result) {
+        ArticulatedLimb limb = null; AnimateMesh mesh = null; Body meshBody = null;
+        Set<Edge> edges = null; Set<MeshTriangle> tris = null;
+        if (mesh1 instanceof ArticulatedLimb) {
+            limb = (ArticulatedLimb) mesh1; mesh = mesh2; meshBody = body2;
+            edges = busyEdges(body2Triangles); tris = body2Triangles;
+        } else if (mesh2 instanceof ArticulatedLimb) {
+            limb = (ArticulatedLimb) mesh2; mesh = mesh1; meshBody = body1;
+            edges = busyEdges(body1Triangles); tris = body1Triangles;
+        }
+        if (limb == null) return false;
+        Quaternion orientLimb = limb.getCurrentOrientation().getInverse();
+        Quaternion orientMesh = mesh.getCurrentOrientation().getInverse();
+        Vector3D posLimb = limb.getDynamicState().getCoMPosition();
+        Vector3D posMesh = ((Body.State) mesh.getDynamicState()).getCoMPosition();
+        double dist = 1e20; InequalityConstraint constr = null;
+        for (Pair<Vector3D, Double> bubble : limb.getBubbles()) {
+            Vector3D centre = orientLimb.transform(bubble.getLeft().subtract(posLimb));
+            double radius = bubble.getRight();
+            for (MeshTriangle tri : tris) {
+                Vector3D p1 = bubble.getLeft().add(tri.getNormal().mult(-1000));
+                Vector3D p2 = bubble.getLeft().add(tri.getNormal().mult(+1000));
+                Vector3D pointW = tri.getVertices()[0].getPosition();
+                Vector3D pointL = orientMesh.transform(pointW.subtract(posMesh));
+                double d = bubble.getLeft().subtract(pointW).mult(tri.getNormal()) - radius;
+                if ((d > 0) || (d > dist)) continue;
+                if (tri.lineAgainstTriangle(p1.getComponent(0), p1.getComponent(1), p1.getComponent(2),
+                        p2.getComponent(0), p2.getComponent(1), p2.getComponent(2)) == null) continue;
+                constr = new SphereFaceCollision(limb.getDynamicBody(), centre, radius,
+                        meshBody, pointL, orientMesh.transform(tri.getNormal()));
+                dist = d;
+            }
+            for (Edge edge : edges) {
+                Vector3D p1 = edge.getLeft().getPosition();
+                Vector3D p2 = edge.getRight().getPosition();
+                Vector3D dir = p2.subtract(p1).normalize();
+                double d = bubble.getLeft().subtract(p1).cross(dir).magnitude() - radius;
+                if ((d > 0) || (d > dist)) continue;
+                constr = new SphereEdgeCollision(limb.getDynamicBody(), centre, radius, meshBody,
+                        orientMesh.transform(p1.subtract(posMesh)), orientMesh.transform(dir));
+                dist = d;
+            }
+        }
+        if (constr == null) return false;
+        result.addInteraction(constr);
+        return true;
+    }
+    
     private boolean linesProximity(Vector3D p1, Vector3D p2, Vector3D q1, Vector3D q2) {
         Vector3D s = p2.subtract(p1), t = q2.subtract(q1);
         double ss = s.magnitude()*s.magnitude(), tt = t.magnitude()*t.magnitude();
@@ -181,22 +297,26 @@ public class Collision {
         return (lambda >= 0.0) && (lambda <= 1.0);
     }
     
-    // Returns the edges in anticlockwise orientation, as seen when looking at the outside
-    private Set<Pair<MeshVertex,MeshVertex>> triangleEdges(MeshTriangle tri) {
-        Set<Pair<MeshVertex,MeshVertex>> result = new java.util.HashSet<Pair<MeshVertex,MeshVertex>>();
-        Vector3D p1 = tri.getVertices()[0].getPosition();
-        Vector3D p2 = tri.getVertices()[1].getPosition();
-        Vector3D p3 = tri.getVertices()[2].getPosition();
-        if (p2.subtract(p1).cross(p3.subtract(p1)).mult(tri.getNormal()) >= 0.0) {
-            result.add(new Pair<MeshVertex,MeshVertex>(tri.getVertices()[0], tri.getVertices()[1]));
-            result.add(new Pair<MeshVertex,MeshVertex>(tri.getVertices()[1], tri.getVertices()[2]));
-            result.add(new Pair<MeshVertex,MeshVertex>(tri.getVertices()[2], tri.getVertices()[0]));
-        } else {
-            result.add(new Pair<MeshVertex,MeshVertex>(tri.getVertices()[0], tri.getVertices()[2]));
-            result.add(new Pair<MeshVertex,MeshVertex>(tri.getVertices()[2], tri.getVertices()[1]));
-            result.add(new Pair<MeshVertex,MeshVertex>(tri.getVertices()[1], tri.getVertices()[0]));
+    private Set<Edge> busyEdges(Set<MeshTriangle> triangles) {
+        Set<Edge> once = new HashSet<Edge>(), twice = new HashSet<Edge>();
+        for (MeshTriangle tri : triangles) {
+            MeshVertex v1 = tri.getVertices()[0];
+            MeshVertex v2 = tri.getVertices()[1];
+            MeshVertex v3 = tri.getVertices()[2];
+            Edge edge1 = new Edge(v1, v2, tri);
+            Edge edge2 = new Edge(v2, v3, tri);
+            Edge edge3 = new Edge(v3, v1, tri);
+            if (once.contains(edge1)) {
+                twice.add(edge1); edge1.secondTri = tri;
+            } else once.add(edge1);
+            if (once.contains(edge2)) {
+                twice.add(edge2); edge2.secondTri = tri;
+            } else once.add(edge2);
+            if (once.contains(edge3)) {
+                twice.add(edge3); edge3.secondTri = tri;
+            } else once.add(edge3);
         }
-        return result;
+        return twice;
     }
     
     
@@ -215,6 +335,16 @@ public class Collision {
 
         MeshTriangle getTri2() {
             return (tri1.getBody() == body2) ? tri1 : tri2;
+        }
+    }
+    
+    
+    private static class Edge extends CommutativePair<MeshVertex> {
+        private MeshTriangle firstTri, secondTri = null;
+        
+        Edge(MeshVertex v1, MeshVertex v2, MeshTriangle firstTri) {
+            super(v1, v2);
+            this.firstTri = firstTri;
         }
     }
 }
