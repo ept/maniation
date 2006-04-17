89871474a89ebfe551b381f6af4d94dbca0f03d8
Martin Kleppmann <martin@kleppmann.de>
2006-04-17 00:35:38 +0000

    Tiny fix
    git-svn-id: file:///Users/martin/Documents/svnrepo/trunk/martin/graphics/maniation@1495 3ab4d77f-209f-454f-af32-ffe4b78d429c

--- de/kleppmann/maniation/maths/RungeKutta.java
+++ de/kleppmann/maniation/maths/RungeKutta.java
@@ -6,7 +6,7 @@ public class RungeKutta implements ODESolver {
     
     private ODE ode;
     private double time;
-    private double h, hnew, hmin = 0.0001, hmax = 0.1, eps = 1e-6, errorOffset = 0.01, safety = 0.9;
+    private double h, hnew, hmin = 0.00001, hmax = 0.1, eps = 1e-6, errorOffset = 0.01, safety = 0.9;
     private double shrinkPower = -0.25, growPower = -0.2;
     private Vector status, error;
     private boolean colliding = false;
@@ -99,7 +99,7 @@ public class RungeKutta implements ODESolver {
             if (time + h > finishTime) h = finishTime - time;
             try {
                 boolean allowBacktrack = false;//(h >= 1.1*hmin);
-                status = ode.timeStep(time, calcStep(allowBacktrack), allowBacktrack);
+                status = ode.timeStep(time + h, calcStep(allowBacktrack), allowBacktrack);
                 if (allowBacktrack) System.out.println("completed.");
                 else System.out.println("completed (penetration ignored).");
                 time += h; h = hnew; colliding = false;
