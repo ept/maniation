6e540bedb9021d08135d4c95049beb7a74c3f62c
Martin Kleppmann <martin@kleppmann.de>
2006-04-17 00:36:23 +0000

    Be more intolerant
    git-svn-id: file:///Users/martin/Documents/svnrepo/trunk/martin/graphics/maniation@1497 3ab4d77f-209f-454f-af32-ffe4b78d429c

--- de/kleppmann/maniation/maths/ConjugateGradient.java
+++ de/kleppmann/maniation/maths/ConjugateGradient.java
@@ -5,7 +5,7 @@ public class ConjugateGradient {
     private int size;
     private Matrix[] alist;
     private Vector b;
-    private double tolerance = 1e-7;
+    private double tolerance = 1e-17;
     private int maxIter;
     private Method method = Method.SIMPLE;
     
